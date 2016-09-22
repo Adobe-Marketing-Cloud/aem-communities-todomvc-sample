48d9fe062fdfec9e1fe60e975af7835cf1ee906f
Siddharth Palaniswami <palanisw@adobe.com>
2016-09-22 01:32:03 -0700

    add client side rendering for filtering

--- content/jcr_root/apps/scf-todo/components/hbs/todos/clientlibs/todos.js
+++ content/jcr_root/apps/scf-todo/components/hbs/todos/clientlibs/todos.js
@@ -2,14 +2,32 @@
     "use strict";
     var TodoList = SCF.Model.extend({
         modelName: "TodoListModel",
+        relationships: {
+            "items": {
+                collection: "TodoItemsCollection",
+                model: "TodoItemModel"
+            }
+        },
+        filter: function(state) {
+            this.fetch({
+                data: $.param({
+                    filter: state
+                })
+            });
+        },
         addTodo: function(itemText) {
             var that = this;
             var success = function(response) {
                 that.log.debug("added item");
                 that.log.debug(response.response);
-                var items = _.clone(that.get("items")) || [];
-                items.unshift(response.response);
-                that.set({"items":items});
+                var TodoItemKlass = SCF.Models[that.constructor.prototype.relationships.items.model];
+                var newToDo = TodoItemKlass.createLocally(response.response);
+                var items = that.get("items");
+                items.push(newToDo);
+                that.set({
+                    "items": items,
+                    "totalSize": that.get("totalSize") + 1
+                });
             };
             var error = function(e) {
                 that.log.error("Unable to add item");
@@ -31,23 +49,32 @@
         }
     });
 
+    var TodoItemsCollection = Backbone.Collection.extend({
+        collectionName: "TodoItemsCollection"
+    });
+
     var TodoListView = SCF.View.extend({
         viewName: "TodoList",
         init: function() {
             this.listenTo(this.model, "change", this.render);
             this.listenTo(this.model, "destroy", this.destroy);
-        }
+        },
         addTodo: function(e) {
             var text = this.getField("itemText");
             this.model.addTodo(text);
         },
         checkForEnter: function(e) {
-          if (e.keyCode == 13) {
-              e.preventDefault();
-              e.stopImmediatePropagation();
-              this.addTodo(e);
-          }  
+            if (e.keyCode == 13) {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                this.addTodo(e);
+            }
         },
+        filter: function(e) {
+            var state = e.currentTarget.getAttribute("data-filter");
+            SCF.Router.navigate(window.location.pathname + "?filter=" + state);
+            this.model.filter(state);
+        }
     });
 
     SCF.registerComponent('scf-todo/components/hbs/todos', TodoList, TodoListView);
